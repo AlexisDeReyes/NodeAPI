ca64e8d33aa944b54d40b5a2271d086b7c49c2d8
AReyes <alexisreyes@quickenloans.com>
2014-12-11 01:20:27 -0500

    adsfdsfa

--- server.js
+++ server.js
@@ -4,17 +4,16 @@ var logger = require('./logger');
 var Return = require('./returner').Return;
 
 var server = http.createServer(function(request, response){
-	if(request.method == 'OPTIONS')
+	if(request.method == 'OPTIONS'){
 		var headers = {};
-      // IE8 does not allow domains to be specified, just the *
-      // headers["Access-Control-Allow-Origin"] = req.headers.origin;
-      headers["Access-Control-Allow-Origin"] = "*";
-      headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
-      headers["Access-Control-Allow-Credentials"] = false;
-      headers["Access-Control-Max-Age"] = '86400'; // 24 hours
-      headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
-      res.writeHead(200, headers);
-      res.end();
+	    headers["Access-Control-Allow-Origin"] = "*";
+	    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+	    headers["Access-Control-Allow-Credentials"] = false;
+	    headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+	    headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+	    res.writeHead(200, headers);
+	    res.end();
+	}
 	else if(request.method == 'POST' || request.method == 'PUT')
 		RetreiveBodyThenRoute(request, response, route);
 	else
